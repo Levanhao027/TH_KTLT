00876392f6d8fc163c1f2869614130f8fd3b98bf
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 09:06:24 +0700

    cau8.py

---
+++ bai4/cau8.py
@@ -0,0 +1,37 @@
+print("SV: Le van Hao")
+
+print("MSSV: 23575202160027")
+
+longest def(s):
+
+#Longest record
+
+tuDaiNhat =
+
+#Use the split() method to split strings into space-separated words
+
+dsCacTu = s.split()
+
+#Use for loop to iterate through the list of words of the string
+
+for tu in dsCacTu:
+
+#Compare the length of words with the current longest word length
+
+#If the length is equal to the word "DaiNhat", then compare in order by the first letter.
+
+if (len(tu) > len (tuDaiNhat)) or (len (tu) == len (tuDaiNhat) and tu tu
+
+fromDaiNhat = from
+
+return tuDaiNhat
+
+#Enter value from movie
+
+S = input()
+
+#Call the function to pass the required parameters
+
+print (longest_time (s))
+
+ 
