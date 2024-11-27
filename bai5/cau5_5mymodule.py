691685a94b112c91fded6666df19ef8347d24690
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 10:46:59 +0700

    Create cau5_5mymodule.py

---
+++ bai5/cau5_5mymodule.py
@@ -0,0 +1,16 @@
+lưu ý lưu với tên mymodule
+print("Lê Văn Hảo")
+print("235752021610027")
+# mymodule.py
+
+def sort_list(input_list):
+    "Sắp xếp danh sách và trả về danh sách đã sắp xếp."
+    return sorted(input_list)
+
+def find_maximum(input_list):
+    "Tìm và trả về phần tử lớn nhất trong danh sách."
+    return max(input_list)
+
+def find_minimum(input_list):
+    "Tìm và trả về phần tử nhỏ nhất trong danh sách."
+    return min(input_list)
