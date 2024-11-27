686b753a1d93fe556f54b16185758cf32c3b42aa
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 09:23:03 +0700

    cau18.py

---
+++ bai4/cau18.py
@@ -0,0 +1,15 @@
+ 
+print("SV: Le Van Hao")
+print("MSSV: 235752021610027")
+
+def fibonacci list(n):
+    fib list [0, 1]
+    while fib list(-1) + fib list[-2] < n:
+        fib_list.append(fib_list[-1] + fib_list[-2])
+    return fib list
+
+n = nint(input("Nhập số nguyên n: ")
+fib_sequence = fibonacci_list(n)
+print("Danh sách các số Fibonacci nhỏ hơn", n, "là:", fib_sequence)
+
+ 
