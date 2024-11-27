d70ec8e1c441a0e0644140e7531af58c28d33fc5
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 11:32:41 +0700

    cau5_4.py

---
+++ bai5/cau5_4.py
@@ -0,0 +1,10 @@
+print("Sv: Le Van Hao")
+print("MSSV: 23575221610027")
+import numpy as np
+# Tạo mảng với các giá trị từ 12 đến 38
+array = np.arange(12, 39)
+# Đảo ngược mảng
+reversed_array = array[::-1]
+# In kết quả
+print("Mảng ban đầu:", array)
+print("Mảng đảo ngược:", reversed_array)
