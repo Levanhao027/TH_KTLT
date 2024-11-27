02507146eb495da81bcfecd1473c46094ce052a8
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 20:06:31 +0700

    cau5_6.py

--- bai5/cau5_6.py
+++ bai5/cau5_6.py
@@ -1,13 +1,24 @@
+ lưu ý lưu vơi tên main.py
 # main.py
+từ mymodule nhập sort_list, find_maximum, find_minimum
 
-from bubble_sort import bubbleSort
+# Nhập số lượng phần tử trong danh sách
+n = int(input("Nhập số lượng phần tử trong danh sách: "))
 
-# Nhập n phần tử từ bàn phím
-input_list = input("Nhập danh sách các số (cách nhau bởi dấu phẩy): ")
-nlist = [int(x) for x in input_list.split(",")]
+# Nhập các giá trị vào danh sách
+input_list = []
+for i in range(n):
+    value = int(input(f"Nhập phần tử thứ {i + 1}: "))
+    input_list.append(value)
 
 # Sắp xếp danh sách
-sorted_list = bubbleSort(nlist)
+sorted_list = sort_list(input_list)
+
+# Tìm phần tử lớn nhất và nhỏ nhất
+max_value = find_maximum (sorted_list)
+min_value = find_minimum (sorted_list)
 
 # In kết quả
 print("Danh sách đã sắp xếp:", sorted_list)
+print("Phần tử lớn nhất là:", max_value)
+print("Phần tử nhỏ nhất là:", min_value)
