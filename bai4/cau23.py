55a4951f1d8bc46ef5f7c1e1bddcd9a1dc63dc74
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 09:59:38 +0700

    cau23.py

---
+++ bai4/cau23.py
@@ -0,0 +1,18 @@
+def count_letters_and_digits(input_str):
+    letter_count = 0
+    digit_count = 0
+    
+    for char in input_str:
+        if char.isalpha():
+          # Kiểm tra xem ký tự có phải là chữ cái không
+            letter_count += 1
+        elif char.isdigit():
+          # Kiểm tra xem ký tự có phải là chữ số không
+            digit_count += 1
+    return letter_count, digit_count
+# Nhập vào câu từ người dùng
+input_sentence = input("Nhập một câu: ")
+letters, digits = count_letters_and_digits(input_sentence)
+# In kết quả
+print("Số chữ cái là:", letters )
+print("Số chữ số là":", digits)
