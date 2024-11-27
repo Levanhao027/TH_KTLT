de5bf0363e1a8a7e29bd077bb64e783c43b426c4
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 08:13:50 +0700

    cau2_1.py

--- bai2/cau2_1.py
+++ bai2/cau2_1.py
@@ -1,1 +1,9 @@
-
+print("sinh vien: Le Van Hao")
+print("ma so sv: 235752021610027")
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+if n1>n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
+  
