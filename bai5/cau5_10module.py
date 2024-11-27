360e04ef5a1302852a53f9fe30e1f652c53d918b
Levanhao027 <levanhaodc5@gmail.com>
2024-11-27 20:45:55 +0700

    cau5_10module.py

---
+++ bai5/cau5_10module.py
@@ -0,0 +1,8 @@
+def bubblesort (nlist): 
+   
+n=len(nlist)
+for i in range(n)
+   for j in range(0,n - i - 1):
+       if nlist[j] > nlist[j+1]:
+         nlist[j], nlist[j+1]=nlist[j+1], nlist[j]
+return nlist
