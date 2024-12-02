dc522e86c57dc43d7b825c1b47aaca0792bc79d4
Levanhao027 <levanhaodc5@gmail.com>
2024-12-02 08:55:40 +0700

    Add files via upload

---
+++ bai8/b8.4c.py
@@ -0,0 +1,13 @@
+print("Le Van Hao")
+print("235752021610027")
+from tkinter import * 
+window = Tk() 
+window.title(" Chào mừng bạn đến với app LikeGeeks") 
+window.geometry('350x200') 
+lbl = Label(window, text="Xin chào") 
+lbl.grid(column=0, row=0) 
+def clicked(): 
+    lbl.configure(text="Nút đã được nhấp !!") 
+btn = Button(window, text=" Nhấp vào tôi", command=clicked) 
+btn.grid(column=1, row=0) 
+window.mainloop()
