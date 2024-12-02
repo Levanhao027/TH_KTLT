4110c0961b5261ef3d54993894f02443fc81963a
Levanhao027 <levanhaodc5@gmail.com>
2024-12-02 08:55:23 +0700

    Add files via upload

---
+++ bai8/b8.4b.py
@@ -0,0 +1,24 @@
+print("Le Van Hao")
+print("235752021610027")
+import tkinter as tk
+
+# Khởi tạo cửa sổ chính
+window = tk.Tk()
+window.title("Cửa sổ Tkinter Đơn Giản")
+window.geometry("300x200")
+
+# Tạo một nhãn và nút bấm
+label = tk.Label(window, text="Xin chào Tkinter!")
+label.pack(pady=20)
+
+button = tk.Button(window, text="Mở", command=lambda: label.config(text="Đã mở!"))
+button.pack(pady=10)
+
+button = tk.Button(window, text="Đóng", command=lambda: label.config(text="Đã đóng!"))
+button.pack(pady=10)
+ 
+
+# Chạy vòng lặp chính của Tkinter
+window.mainloop()
+
+
